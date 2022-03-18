a834b769858b4607ef02ea1c6fcdf3a50572facd
Stephani Rocha <gomes.stephani19@gmail.com>
2022-03-18 17:36:27 -0300

    Implementa a funçao getRelatedEmployees

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,28 @@
 const data = require('../data/zoo_data');
 
+const collaborators = data.employees;
+
 function isManager(id) {
-  // seu código aqui
+  const objManager = collaborators.some((elementSome) => elementSome.managers.includes(id));
+  return objManager;
 }
 
+isManager();
+
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === true) {
+    const responsavel = collaborators.filter((element) => element.managers.includes(managerId));
+    const concatName = responsavel.reduce((acc, item) => {
+      const name = `${item.firstName} ${item.lastName}`;
+      acc.push(name);
+      return acc;
+    }, []);
+
+    return concatName;
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
+
+console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 
 module.exports = { isManager, getRelatedEmployees };
