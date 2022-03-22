1369669f1978c7b3e1e8485e5eecec985c9dc17e
Stephani Rocha <gomes.stephani19@gmail.com>
2022-03-22 11:27:30 -0300

    Implementa a funçao calculateEntry - Requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,39 @@
+const { prices } = require('../data/zoo_data');
+
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.values(entrants).length === 0) {
+    return 0;
+  }
+
+  const objectPeoples = {};
+
+  const returnAges = entrants.map((element) => element.age);
+  const childAge = returnAges.filter((elementFilter) => elementFilter < 18);
+  const adultAge = returnAges.filter((elementFilter) => elementFilter >= 18 && elementFilter < 50);
+  const seniorAge = returnAges.filter((elementFilter) => elementFilter >= 50);
+
+  objectPeoples.child = childAge.length;
+  objectPeoples.adult = adultAge.length;
+  objectPeoples.senior = seniorAge.length;
+
+  return objectPeoples;
 }
+console.log(countEntrants({}));
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.values(entrants).length === 0) {
+    return 0;
+  }
+
+  const child = countEntrants(entrants).child * prices.child;
+  const adult = countEntrants(entrants).adult * prices.adult;
+  const senior = countEntrants(entrants).senior * prices.senior;
+
+  return child + adult + senior;
 }
+
+console.log(calculateEntry({}));
 
 module.exports = { calculateEntry, countEntrants };
