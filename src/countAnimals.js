1f7f9b8a96a7600df3a638c0d0b251f41f776e5c
Stephani Rocha <gomes.stephani19@gmail.com>
2022-03-23 11:35:16 -0300

    Implementa a funçao countAnimal - Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,21 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    const animalUndefined = data.species.reduce((acc, element) => {
+      acc[element.name] = element.residents.length;
+      return (acc);
+    }, {});
+    return animalUndefined;
+  }
+  const foundAnimal = data.species.find((specie) => animal.specie === specie.name);
+  if (Object.keys(animal).includes('sex')) {
+    const animalSex = foundAnimal.residents.filter((resident) => resident.sex === animal.sex);
+    return animalSex.length;
+  }
+  return foundAnimal.residents.length;
 }
+
+console.log(countAnimals({ specie: 'lions' }));
 
 module.exports = countAnimals;
